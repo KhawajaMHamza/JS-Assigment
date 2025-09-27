2ecbb4e5f63309dc84ec7424b2349faba6c40223
KhawajaMHamza <mhamzakhawaja007@gmail.com>
2025-09-27 17:10:12 +0500

    Add files via upload

---
+++ JS Assigment 8/app.js
@@ -0,0 +1,31 @@
+// chapter 47-52
+// Question 1 and 2
+// function Link(element) {
+//     alert("Hello World");
+//     var a = alert("You clicked on image with id: " + element.id);
+// }
+// function ima(element) {
+//     alert("Hello World");
+        
+//         var b = alert("You clicked on image with id: " + element.id);
+// }
+// function im(element) {
+//     alert("Hello World");
+   
+//     var c = alert("You clicked on image with id: " + element.id);
+// }
+
+// Question 3
+// function deleteRow(r) {
+//     var i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+// }
+// Next Chapter
+// Question 1
+// function submitForm(){
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     console.log("Name: " + name);
+//     console.log("Email: " + email);
+// }
+// The End
